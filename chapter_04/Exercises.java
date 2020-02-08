d24a020dd3362e4de43229cd410f4f75b3ed2437
Carmen <carmen.pomana04@gmail.com>
2020-02-08 14:09:46 +0100

    Updated exercise 1

--- chapter_04/Exercises.java
+++ chapter_04/Exercises.java
@@ -4,30 +4,61 @@ public class Exercises {
 		// Write code to determine wether the toString and substring 
 		// methods of StringBuilder and String class return an interned 
 		// string or not
+		System.out.println("Exercise 1");
+		exercise1();
+		System.out.println("_".repeat(80));
+		
+		// "12345678" -> XXXX5678 (last 4 the original char, rest X)
+		if (args.length > 0) {
+			System.out.println("Exercise 2");
+			exercise2(args[0]);
+			System.out.println("_".repeat(80));
+		}
+	}
+	
+	static void exercise1() {
+		
 		
 		// String -> toString()
 		String stringObject = new String("hello");
-		System.out.printf("is toString method of String interned? %b\n",  
-			stringObject.toString() == "hello"); // false
+		System.out.printf("is toString method of String interned? %b-%b\n",  
+			stringObject.toString() == "hello",
+			stringObject.toString() == stringObject.intern()); // false
 		
 		// String -> substring(start, end);
 		String subStringObject = stringObject.substring(0,2);
-		System.out.printf("is substring method of String interned? %b\n",  
-			subStringObject == "he");
+		System.out.printf("is substring method of String interned? %b-%b\n",  
+			subStringObject == "he",
+			subStringObject == subStringObject.intern());
+		System.out.printf("is subStringObject.intern() equal to literal string? %b\n", subStringObject.intern()=="he");
 
 		// String -> toString()
 		String literal = "hello";
-		System.out.printf("is toString method of literal interned? %b\n",  
-			literal.toString() == "hello"); // true
-		System.out.printf("is substring method of literal interned? %b\n",  
-			literal.substring(0,2) == "he");
+		System.out.printf("is toString method of literal interned? %b-%b\n",  
+			literal.toString() == "hello",
+			literal == literal.intern()); // true
+		String literalSubstring = literal.substring(0,2);
+		System.out.printf("is substring method of literal interned? %b-%b\n",  
+			literalSubstring == "he",
+			literalSubstring == literalSubstring.intern());
 		
 		StringBuilder stringBuilder = new StringBuilder("hello");
-		System.out.printf("is toString method of StringBuilder interned? %b\n", 
-			stringBuilder.toString() == "hello"); // false
+		System.out.printf("is toString method of StringBuilder interned? %b-%b\n", 
+			stringBuilder.toString() == "hello", 
+			stringBuilder.toString() == stringBuilder.toString().intern()); // false
 		
 		String subStringBuilder = stringBuilder.substring(0,2);
-		System.out.printf("is substring method of StringBuilder interned? %b\n", 
-			subStringBuilder.toString() == "he");
+		System.out.printf("is substring method of StringBuilder interned? %b-%b\n", 
+			subStringBuilder.toString() == "he", 
+			subStringBuilder.toString() == subStringBuilder.toString().intern());
+	}
+
+	static void exercise2(String input) {
+		char[] chars = input.toCharArray();
+		int len = chars.length - 4;
+		char[] x = new char[len];
+		for (int i = 0; i < len; i++) {
+			
+		}
 	}
 }
